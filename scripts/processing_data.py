38f23b17312df60bcec987f2e81822314dd3acbf
VuLemon <vule@brandeis.edu>
2025-07-23 18:22:03 -0400

    Implemented bike dataset downloading as part of processing_data.py

--- scripts/processing_data.py
+++ scripts/processing_data.py
@@ -6,7 +6,8 @@ import shutil
 from pathlib import Path
 from pyspark.ml.feature import OneHotEncoder, MinMaxScaler, VectorAssembler, StringIndexer
 import os
-
+import subprocess
+import zipfile
 
 spark = SparkSession.builder.appName("myApp").getOrCreate()
 
@@ -14,16 +15,98 @@ bike_folder_path = "./historical_datasets/Bike Historical Data"
 
 weather_path = "./historical_datasets/Weather Historical Data/weather_data.csv"
 
-bike_file_list = [os.path.join(bike_folder_path, f) for f in os.listdir(bike_folder_path) if f.endswith(".csv")] # Makes sure it's a CSV file
 
 
+## MAIN METHOD OF SCRIPT ##
+def main():
+    generate_bike_files()
+    weather_df = load_weather_data()
+    weather_df = broadcast(weather_df)
+    process_bike_files(weather_df)
+    batch_paths = sorted(str(p) for p in Path("./processed_dataset").glob("batch_*"))
+
+    combined_df = None
+
+    for path in batch_paths:
+        df = spark.read.option("header", True).parquet(path)
+        if combined_df is None:
+            combined_df = df
+        else:
+            combined_df = combined_df.unionByName(df)
+
+    combined_df = combined_df.coalesce(1)
+    combined_df.write.mode("overwrite").option("header", True).parquet("./intermediate")
+    final_df = spark.read.parquet("./intermediate", header=True)
+    save_processed_data(final_df)
+    save_raw_data(final_df)
+    if os.path.exists("./processed_dataset"):
+        shutil.rmtree("./processed_dataset")
+    if os.path.exists("./intermediate"):
+        shutil.rmtree("./intermediate")
+    
+
+
+
+
+## GENERATES HISTORICAL BIKE DATA ##
+def generate_bike_files():
+    bash_command = """
+    curl -L -o ~/Downloads/citi-bike-stations.zip \\
+    https://www.kaggle.com/api/v1/datasets/download/rosenthal/citi-bike-stations
+    """
+    subprocess.run(bash_command, shell=True, check=True)
+
+    zip_path = os.path.expanduser("~/Downloads/citi-bike-stations.zip")
+    extract_to = "./historical_datasets/Bike Historical Data"
+    print("Extracting dataset...")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+
+
+
+
+## FEATURE ENGINEERING FOR WEATHER DATA ##
+def load_weather_data():
+    weather_df = spark.read.csv(weather_path, header=True)
+    weather_df = clean_weather_table(weather_df)
+    return weather_df
+
+def clean_weather_table(df):
+    df = df.filter(col("HourlyDryBulbTemperature").isNotNull()).select("DATE", "HourlyDryBulbTemperature")
+    df = truncate_time_weather(df)
+    df = cast_temperature_as_integer(df)
+    return df
+    
+def truncate_time_weather(df):
+    df = df.withColumn("time", to_timestamp(df.DATE))
+    df = df.withColumn("time", date_trunc("hour", "time"))
+    return df
+
+def cast_temperature_as_integer(df):
+    df = df.withColumn("HourlyDryBulbTemperature", regexp_replace("HourlyDryBulbTemperature",'s$','')) #Some number ends in s, like 74s for some reason
+    df = df.withColumn("HourlyDryBulbTemperature", col("HourlyDryBulbTemperature").cast(IntegerType()))
+    return df.groupBy("time").agg(avg("HourlyDryBulbTemperature"))
+
+
+
+
+
+## FEATURE ENGINEERING FOR BIKE DATA ##
 def process_bike_files(weather_df):
+    print("check for process_bike_files")
+    bike_file_list = retrieve_bike_file_lists()
+
     for i, file in enumerate(bike_file_list):
+        print(f"loading {file} into Spark")
         bike_df = load_bike_data(file)
         joined_data = bike_df.join(weather_df, on=["time"])
         joined_data.printSchema()
         joined_data.show(1, truncate=False)
         joined_data.write.mode("overwrite").option("header", True).parquet(f"./processed_dataset/batch_{i}")
+
+def retrieve_bike_file_lists():
+    return [os.path.join(bike_folder_path, f) for f in os.listdir(bike_folder_path) if f.endswith(".csv")]
 
 def load_bike_data(file):
     df = spark.read.csv(file,header = True)
@@ -66,30 +149,10 @@ def truncate_time_bike(df):
     # return availability
 
 
-def load_weather_data():
-    weather_df = spark.read.csv(weather_path, header=True)
-    weather_df = clean_weather_table(weather_df)
-    return weather_df
-
-def clean_weather_table(df):
-    df = df.filter(col("HourlyDryBulbTemperature").isNotNull()).select("DATE", "HourlyDryBulbTemperature")
-    df = truncate_time_weather(df)
-    df = cast_temperature_as_integer(df)
-    return df
-    
-def truncate_time_weather(df):
-    df = df.withColumn("time", to_timestamp(df.DATE))
-    df = df.withColumn("time", date_trunc("hour", "time"))
-    return df
-
-def cast_temperature_as_integer(df):
-    df = df.withColumn("HourlyDryBulbTemperature", regexp_replace("HourlyDryBulbTemperature",'s$','')) #Some number ends in s, like 74s for some reason
-    df = df.withColumn("HourlyDryBulbTemperature", col("HourlyDryBulbTemperature").cast(IntegerType()))
-    return df.groupBy("time").agg(avg("HourlyDryBulbTemperature"))
 
 
 
-
+## ENCODES AND SAVES DATA AS SPARK DFs ##
 def save_processed_data(df):
     df = df.withColumn("hour", hour("time"))
     df = df.withColumn("weekday", date_format("time", "EEEE"))
@@ -109,7 +172,6 @@ def save_processed_data(df):
         "station_id_index"
     )
     df.write.mode("overwrite").parquet("./final_output/cleaned_data")
-
 
 def normalize_temperature(df):
     df = df.withColumn("avg(HourlyDryBulbTemperature)", col("avg(HourlyDryBulbTemperature)").cast(DoubleType()))
@@ -147,42 +209,9 @@ def one_hot_encode_data(df):
     print("✅ Finished one-hot encoding with readable column names")
     return df_encoded
 
-
 def save_raw_data(df):
     df.write.mode("overwrite").option("header", True).parquet("./final_output/raw_data")
 
-
-
-def main():
-    weather_df = load_weather_data()
-    weather_df = broadcast(weather_df)
-    process_bike_files(weather_df)
-    batch_paths = sorted(str(p) for p in Path("./processed_dataset").glob("batch_*"))
-
-    # Read and union in batches
-    combined_df = None
-
-    for path in batch_paths:
-        df = spark.read.option("header", True).parquet(path)
-        if combined_df is None:
-            combined_df = df
-        else:
-            combined_df = combined_df.unionByName(df)
-
-    # Optional: coalesce to reduce output file count
-    combined_df = combined_df.coalesce(1)
-
-    # Write to final output directory
-    combined_df.write.mode("overwrite").option("header", True).parquet("./intermediate")
-
-    final_df = spark.read.parquet("./intermediate", header=True)
-    save_processed_data(final_df)
-    save_raw_data(final_df)
-    if os.path.exists("./processed_dataset"):
-        shutil.rmtree("./processed_dataset")
-    if os.path.exists("./intermediate"):
-        shutil.rmtree("./intermediate")
-    
     
 
 main()
